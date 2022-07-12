cbf7c0faa53a589d0e0fdda26387a908aab329a1
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2022-07-12 15:27:05 +0100

    ADDED: changelog update on release

--- .github/workflows/update_version.py
+++ .github/workflows/update_version.py
@@ -1,6 +1,8 @@
+from hashlib import new
 import re
 import sys
 from pathlib import Path
+import datetime
 
 # load plugin file
 txt = Path('sim-plugin.php').read_text()
@@ -15,4 +17,22 @@ txt = txt.replace(oldVersion, newVersion)
 # Write changes
 f = open('sim-plugin.php', "w")
 f.write(txt)
+f.close()
+
+# Update the changelog with the new release
+
+file    = 'CHANGELOG.md'
+
+# load plugin file
+changelog = Path(file).read_text()
+
+unreleased   = re.search(r'(## \[Unreleased\] - yyyy-mm-dd)', changelog).group(1)
+
+new            = "## [Unreleased] - yyyy-mm-dd\n\n### Added\n\n### Changed\n\n### Fixed\n\n## [" + newVersion + "] - " + datetime.datetime.now().strftime("%Y-%m-%d")
+
+changelog = changelog.replace(unreleased, new)
+
+# Write changes
+f = open(file, "w")
+f.write(changelog)
 f.close()
