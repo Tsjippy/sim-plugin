9121994592cd51e064ccd5e8a87338e4b8cfd6a7
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2022-08-25 20:04:15 +0100

    skip: versiion bump

--- includes/modules/frontendPosting/php/__module_menu.php
+++ includes/modules/frontendPosting/php/__module_menu.php
@@ -2,7 +2,7 @@
 namespace SIM\FRONTENDPOSTING;
 use SIM;
 
-const MODULE_VERSION		= '7.0.20';
+const MODULE_VERSION		= '7.0.21';
 //module slug is the same as grandparent folder name
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
