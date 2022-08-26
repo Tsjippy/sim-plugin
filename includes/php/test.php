5445e61a05943dcdcc203b3f85e048676dcc56da
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2022-08-26 13:44:52 +0100

    skip: test

--- includes/php/test.php
+++ includes/php/test.php
@@ -8,24 +8,5 @@ add_shortcode("test",function ($atts){
 	return 'e-mail send';
 });
 
-function sendTestEmail(){
-    wp_mail('enharmsen@gmail.com' , 'test e-mail', 'test');
- 
-    return 'E-mail send';
-}
-
-add_action( 'rest_api_init', function () {
-    // add element to form
-    register_rest_route(
-        'sim/v1',
-        '/sendtestemail',
-        array(
-            'methods'               => 'GET',
-            'callback'              =>  __NAMESPACE__.'\sendTestEmail',
-            'permission_callback'   => '__return_true',
-        )
-    );
-});
-
-// turn off incorrect error
+// turn off incorrect error on localhost
 add_filter( 'wp_mail_smtp_core_wp_mail_function_incorrect_location_notice', '__return_false' );
