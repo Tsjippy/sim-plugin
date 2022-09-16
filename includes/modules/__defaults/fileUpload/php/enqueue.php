58856ccb2070da7c78191b023739b153f986365f
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2022-09-16 10:53:14 +0100

    fixed: problem in file upload file versioning

--- includes/modules/__defaults/fileUpload/php/enqueue.php
+++ includes/modules/__defaults/fileUpload/php/enqueue.php
@@ -4,5 +4,5 @@ use SIM;
 
 add_action( 'wp_enqueue_scripts', function(){
     //File upload js
-	wp_register_script('sim_fileupload_script', plugins_url('js/fileupload.min.js', __DIR__), array('sim_formsubmit_script', 'sim_purify'), STYLE_VERSION, true);
+	wp_register_script('sim_fileupload_script', plugins_url('js/fileupload.min.js', __DIR__), array('sim_formsubmit_script', 'sim_purify'), MODULE_VERSION, true);
 }, 1);
