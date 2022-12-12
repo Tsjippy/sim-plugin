76950ff72a33d3b8ea0ca8eaae43a7f368551397
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2022-12-12 15:25:27 +0100

    skip: version bump

--- includes/modules/forms/php/__module_menu.php
+++ includes/modules/forms/php/__module_menu.php
@@ -2,7 +2,7 @@
 namespace SIM\forms;
 use SIM;
 
-const MODULE_VERSION		= '7.0.71';
+const MODULE_VERSION		= '7.0.72';
 //module slug is the same as grandparent folder name
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
