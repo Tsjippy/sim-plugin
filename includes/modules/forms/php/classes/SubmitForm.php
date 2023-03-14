b7455a5bafa9caaa164764cf22750f35ddf54873
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2023-03-14 10:29:14 +0100

    fixed: only replace exact match with date

--- includes/modules/forms/php/classes/SubmitForm.php
+++ includes/modules/forms/php/classes/SubmitForm.php
@@ -167,7 +167,7 @@ class SubmitForm extends SimForms{
 			}else{
 				if(is_array($replaceValue)){
 					$replaceValue	= implode(',', $replaceValue);
-				}elseif(preg_match('/(\d{4}-\d{2}-\d{2})/', $replaceValue, $matches)){
+				}elseif(preg_match('/^(\d{4}-\d{2}-\d{2})$/', $replaceValue, $matches)){
 					$replaceValue	= date(get_option('date_format'), strtotime((string)$matches[1]));
 				}
 				//replace the placeholder with the value
