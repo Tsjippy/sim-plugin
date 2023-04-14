3a51ea1b700b2da99b95571488f4d8f42386d49b
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2023-04-14 11:54:39 +0100

    fixed: upcoming arrivals block

--- includes/modules/events/blocks/upcomingArrivals/build/index.asset.php
+++ includes/modules/events/blocks/upcomingArrivals/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f22871a26f1259fe95f3');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e9c2ca54cdb835798463');
--- includes/modules/forms/blocks/formresults/build/index.asset.php
+++ includes/modules/forms/blocks/formresults/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e68b3c03f054eca55ee6');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b6c0e1e536302ed1b5d3');
