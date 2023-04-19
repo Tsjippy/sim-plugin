aedf92bd6f7d85f6b2dbf108835bcc958ff2b647
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2023-04-19 11:48:40 +0100

    fixed: insert vimeo shortcode

--- includes/modules/vimeo/php/tiny_mce.php
+++ includes/modules/vimeo/php/tiny_mce.php
@@ -6,9 +6,7 @@ use SIM;
 //change hyperlink to shortcode for vimeo videos
 add_filter( 'media_send_to_editor', function ($html, $id, $attachment) {
 	if(strpos($attachment['url'], 'vimeo.com') !== false){
-		$vimeoId	= str_replace('https://vimeo.com/', '', $attachment['url']);
-
-		$vimeoId	= explode('?', $vimeoId)[0];
+		$vimeoId	= get_post_meta($id, 'vimeo_id', true);
 
 		$html		= "[vimeo_video id=$vimeoId]";
 	}
