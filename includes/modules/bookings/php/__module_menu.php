1cb47bdaea232f1ff97cba52bc94fdb28ae26373
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2024-03-20 09:43:41 +0100

    skip:

--- includes/modules/bookings/php/__module_menu.php
+++ includes/modules/bookings/php/__module_menu.php
@@ -64,7 +64,7 @@ add_filter('sim_submenu_description', function($description, $moduleSlug){
 	?>
 	<p>
 		This module is dependend on the events and forms module.<br>
-		It adds the possibility to book something thrue a form.<br>
+		It adds the possibility to book something via a form.<br>
 		It will display a calendar showing available dates
 	</p>
 
