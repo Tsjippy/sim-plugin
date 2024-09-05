c6f744ad128bca09ba3d9c3e40fb8a982c9513fb
Tsjippy <ewaldha@hotmail.com>
2024-09-05 13:56:52 +0100

    skip:

--- includes/modules/forms/php/__module_menu.php
+++ includes/modules/forms/php/__module_menu.php
@@ -2,7 +2,7 @@
 namespace SIM\forms;
 use SIM;
 
-const MODULE_VERSION		= '7.1.4';
+const MODULE_VERSION		= '7.1.5';
 //module slug is the same as grandparent folder name
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
