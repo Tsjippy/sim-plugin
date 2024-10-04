befc61eade96a8dc4a28978bd7387cced36e4d27
Tsjippy <ewaldha@hotmail.com>
2024-10-04 09:33:21 +0100

    changed: page gallery module to dedicated repo

--- includes/modules/pageGallery/php/enqueue.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace SIM\PAGEGALLERY;
-use SIM;
-
-add_action( 'wp_enqueue_scripts', function(){
-	wp_register_script('sim_page_gallery_script', plugins_url('js/page_gallery.min.js', __DIR__), array('sim_formsubmit_script'), MODULE_VERSION, true);
-});
