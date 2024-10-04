b573353dcf79cd4f97e406b48251bb32f00d51af
Tsjippy <ewaldha@hotmail.com>
2024-10-04 09:20:43 +0100

    changed: embedpage module to dedicated repo

--- includes/modules/embedPage/blocks/embedExternalPage/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a3c5653633a5f47f3a93');
