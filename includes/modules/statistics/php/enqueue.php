015f47d7e60645dbbe85c030cf4c2e3e9270de07
Tsjippy <ewaldha@hotmail.com>
2024-10-04 09:35:46 +0100

    changed: statistics module to dedicated repo

--- includes/modules/statistics/php/enqueue.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace SIM\STATISTICS;
-use SIM;
-
-add_action('wp_enqueue_scripts', function(){
-    //Load js
-    wp_enqueue_script('sim_statistics_script', plugins_url('js/statistics.min.js', __DIR__), array(), MODULE_VERSION,true);
-});
