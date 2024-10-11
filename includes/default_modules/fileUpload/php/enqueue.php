c20cdd93c5a914ed5787c1bfe88277fb64abc762
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:14:16 +0100

    skip:

--- includes/default_modules/fileUpload/php/enqueue.php
+++ includes/default_modules/fileUpload/php/enqueue.php
@@ -5,7 +5,6 @@ use SIM;
 add_action( 'wp_enqueue_scripts', __NAMESPACE__.'\registerUploadScripts', 1);
 
 function registerUploadScripts(){
-    $url=plugins_url('js/fileupload.min.js', __DIR__);
     //File upload js
     wp_register_script('sim_fileupload_script', plugins_url('js/fileupload.min.js', __DIR__), array('sim_formsubmit_script', 'sim_purify'), MODULE_VERSION, true);
 
